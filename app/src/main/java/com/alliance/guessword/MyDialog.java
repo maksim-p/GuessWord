478bd5ac16e0716c0a5fe3c3b4d39fcadd167425
VyacheslavPonomarev <v.v.ponomaryov@yandex.ru>
2019-03-15 16:18:54 +0300

    Edit dialog

--- app/src/main/java/com/alliance/guessword/MyDialog.java
+++ app/src/main/java/com/alliance/guessword/MyDialog.java
@@ -8,6 +8,7 @@ import android.support.v4.app.DialogFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 
@@ -15,6 +16,7 @@ public class MyDialog extends DialogFragment {
     View dialogView;
     EditText editTeam;
     TextView teamName;
+    ImageView changeName;
 
     int viewId;
 
@@ -30,19 +32,24 @@ public class MyDialog extends DialogFragment {
         LayoutInflater inflater = getActivity().getLayoutInflater();
         dialogView = inflater.inflate(R.layout.dialog, null);
 
+        changeName = dialogView.findViewById(R.id.changeName);
+
+        changeName.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editTeam.clearComposingText();
+                teamName.setText(editTeam.getText());
+                onDestroyView();
+            }
+        });
+
         editTeam = dialogView.findViewById(R.id.editTeam);
         editTeam.setText(teamName.getText());
+        editTeam.setSelection(editTeam.getText().length());
 
         builder.setView(dialogView);
         Dialog dialog = builder.create();
-
-        dialog.setCanceledOnTouchOutside(true);
+        dialog.setCanceledOnTouchOutside(false);
         return dialog;
-    }
-
-    @Override
-    public void onCancel(DialogInterface dialog) {
-        super.onCancel(dialog);
-        teamName.setText(editTeam.getText());
     }
 }
